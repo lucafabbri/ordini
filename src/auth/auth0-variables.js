3ecfcfed29e5c18bb2940954abdb849f87668554
Luca Fabbri <luca@zepfiro.com>
2019-03-14 09:58:34 +0100

    callback auth

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
     clientId: '4xZBp25677Wqx9WzzhRRPCY5ylFolYD9',
     domain: 'zepfiro.eu.auth0.com',
-    callbackUrl: 'http://localhost:8081/callback'
+    callbackUrl: 'https://ordini.zepfiro.com/callback'
   }
